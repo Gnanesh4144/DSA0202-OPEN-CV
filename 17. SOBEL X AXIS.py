f7ce72a6223b5f8c7d868a1c9d0cda3893bd6eb8
Gnanesh4144 <113585760+Gnanesh4144@users.noreply.github.com>
2023-05-12 13:07:21 +0530

    Add files via upload

---
+++ 17. SOBEL X AXIS.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('C:\\Users\\gnane\\Pictures\\KKK.jpg',0)
+
+# Output dtype = cv2.CV_8U
+sobel_x = cv2.Sobel(img,cv2.CV_8U,1,0,ksize=5)
+
+cv2.imwrite('sobel_x.jpg',sobel_x)
---
+++ 18. SOBEL Y AXIS.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('C:\\Users\\gnane\\Pictures\\KKK.jpg',0)
+
+# Output dtype = cv2.CV_8U
+sobel_y = cv2.Sobel(img,cv2.CV_8U,0,1,ksize=5)
+
+cv2.imshow('sobel_y',sobel_y)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
